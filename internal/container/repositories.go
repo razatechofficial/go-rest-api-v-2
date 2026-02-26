953f5b80e03e9e42d63eaf588cc4bc56d36ccdfa
razatechofficial <razatechofficial@users.noreply.github.com>
2026-02-26 17:14:24 +0500

    register order repo in container

--- internal/container/repositories.go
+++ internal/container/repositories.go
@@ -1,23 +1,19 @@
-// internal/container/repositories.go
 package container
 
-import "github.com/razatechofficial/go-rest-api-v-2/internal/core/user"
-
-// future: "myapp/internal/core/order"
-// future: "myapp/internal/core/product"
+import (
+	"github.com/razatechofficial/go-rest-api-v-2/internal/core/order"
+	"github.com/razatechofficial/go-rest-api-v-2/internal/core/user"
+)
 
 // Repositories holds all data access layer implementations.
-// All fields typed as interfaces — never concrete types.
 type Repositories struct {
-	User user.Repository
-	// Order   order.Repository
-	// Product product.Repository
+	User  user.Repository
+	Order order.Repository
 }
 
 func (c *Container) buildRepositories() *Repositories {
 	return &Repositories{
-		User: user.NewRepository(c.DB),
-		// Order:   order.NewRepository(c.DB),
-		// Product: product.NewRepository(c.DB),
+		User:  user.NewRepository(c.DB),
+		Order: order.NewRepository(c.DB),
 	}
 }
