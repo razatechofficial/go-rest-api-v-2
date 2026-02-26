9f9054b4a9b28a81db7b340abc13fdd11b14803c
razatechofficial <razatechofficial@users.noreply.github.com>
2026-02-26 17:14:02 +0500

    registered order service in container

--- internal/container/services.go
+++ internal/container/services.go
@@ -1,24 +1,19 @@
-// internal/container/services.go
 package container
 
-import "github.com/razatechofficial/go-rest-api-v-2/internal/core/user"
-
-// future: "myapp/internal/core/order"
+import (
+	"github.com/razatechofficial/go-rest-api-v-2/internal/core/order"
+	"github.com/razatechofficial/go-rest-api-v-2/internal/core/user"
+)
 
 // Services holds all business logic layer implementations.
-// Services receive repository interfaces — never concrete repo types.
 type Services struct {
-	User user.Service
-	// Order   order.Service
-	// Product product.Service
+	User  user.Service
+	Order order.Service
 }
 
 func (c *Container) buildServices() *Services {
 	return &Services{
-		User: user.NewService(c.Repositories.User),
-		// Order: order.NewService(
-		//     c.Repositories.Order,
-		//     c.cfg,
-		// ),
+		User:  user.NewService(c.Repositories.User),
+		Order: order.NewService(c.Repositories.Order),
 	}
 }
